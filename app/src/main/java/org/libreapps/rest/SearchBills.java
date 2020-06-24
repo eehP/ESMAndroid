6e33feac828d93bdd131128ff533a7d212a893fd
Félicien Putinier <fel.putinier@gmail.com>
2020-06-24 16:46:10 +0200

    Add SearchBill content

--- app/src/main/java/org/libreapps/rest/SearchBills.java
+++ app/src/main/java/org/libreapps/rest/SearchBills.java
@@ -1,6 +1,30 @@
 package org.libreapps.rest;
 
-import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
 
-public class SearchBills extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import de.codecrafters.tableview.TableView;
+import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter;
+import de.codecrafters.tableview.toolkit.SimpleTableHeaderAdapter;
+
+public class SearchBills extends AppCompatActivity {
+
+    TableView<String[]>  tb;
+    ProductTableModel tableModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_summary);
+
+        //TABLEVIEW
+        tableModel = new ProductTableModel();
+        tb = (TableView<String[]>) findViewById(R.id.tableView);
+        tb.setColumnCount(4);
+        tb.setHeaderBackgroundColor(Color.parseColor("#03DAC5"));
+        tb.setHeaderAdapter(new SimpleTableHeaderAdapter(this, tableModel.getProductHeaders()));
+        tb.setDataAdapter(new SimpleTableDataAdapter(this, tableModel.getProducts()));
+    }
 }

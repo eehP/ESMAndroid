e23f7e90c5a18c71396c20354823f232030fb726
Gogbox <apiperaud@gmail.com>
2020-06-26 19:42:04 +0200

    Page completed without same scrollbar design

--- app/src/main/java/org/libreapps/rest/SummaryBills.java
+++ app/src/main/java/org/libreapps/rest/SummaryBills.java
@@ -2,11 +2,25 @@ package org.libreapps.rest;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Context;
         import android.content.Intent;
         import android.graphics.Color;
+        import android.graphics.drawable.Drawable;
         import android.os.Bundle;
+        import android.util.Log;
+        import android.view.LayoutInflater;
         import android.view.View;
         import android.widget.Button;
+        import android.widget.LinearLayout;
+        import android.widget.ListView;
+        import android.widget.ScrollView;
+        import android.widget.TextView;
+
+        import org.w3c.dom.Text;
+
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import java.util.List;
 
         import de.codecrafters.tableview.TableView;
         import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter;
@@ -22,13 +36,35 @@ public class SummaryBills extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bill_summary);
 
-        //TABLEVIEW
+        //SCROLL_MESSAGES_VIEW
         tableModel = new ProductTableModel();
-        tb = (TableView<String[]>) findViewById(R.id.tableView);
-        tb.setColumnCount(4);
-        tb.setHeaderBackgroundColor(Color.parseColor("#03DAC5"));
-        tb.setHeaderAdapter(new SimpleTableHeaderAdapter(this, tableModel.getProductHeaders()));
-        tb.setDataAdapter(new SimpleTableDataAdapter(this, tableModel.getProducts()));
+        String[][] products = tableModel.getProducts();
+
+        setContentView(R.layout.bill_summary);
+        LinearLayout m_elementsList = findViewById(R.id.list_elements);
+        findViewById(R.id.last_elements).setVerticalScrollbarPosition(View.SCROLLBAR_POSITION_LEFT);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(60,25,5,25);
+
+        for(int increment = products.length-1; increment >= 0 && increment >= (products.length - 10); increment--){
+            String m_priceCompletion = "";
+            for(int completedSize = 0; completedSize < (40 - products[increment][2].length() - products[increment][0].length()); completedSize++){
+                m_priceCompletion += " ";
+            }
+            String m_data = "\t\t" + products[increment][1].toString() +
+                            "\n\n\t\t" + products[increment][0].toString()  + m_priceCompletion + products[increment][2].toString() + "€";
+
+            TextView m_element = new TextView(this);
+            m_element.setBackgroundResource(R.drawable.text_message_scroll);
+            m_element.setText(m_data);
+            m_element.setTextColor(Color.parseColor("#FFFFFF"));
+            m_element.setTextSize(18);
+            m_element.setLayoutParams(params);
+            m_elementsList.addView(m_element);
+        }
+
+
 
         Button summary = findViewById(R.id.search);
         summary.setOnClickListener(new View.OnClickListener() {

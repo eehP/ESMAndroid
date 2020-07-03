fe62102438b30898a7fa5e412baf4ca6a02f395b
PABERTHIER <39381248+PABERTHIER@users.noreply.github.com>
2020-07-03 14:48:11 +0200

    Correction import

--- app/src/main/java/org/libreapps/rest/SearchBills.java
+++ app/src/main/java/org/libreapps/rest/SearchBills.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
 import android.widget.Button;

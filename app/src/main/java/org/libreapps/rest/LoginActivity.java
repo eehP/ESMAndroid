8cb2caf6ff883c3138d4273d3b95154e19193e67
PABERTHIER <39381248+PABERTHIER@users.noreply.github.com>
2020-07-06 10:19:26 +0200

    Ajout toast

--- app/src/main/java/org/libreapps/rest/LoginActivity.java
+++ app/src/main/java/org/libreapps/rest/LoginActivity.java
@@ -6,19 +6,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.concurrent.ExecutionException;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText userEmail;
     private EditText userPassword;
     private Button buttonlogin;
     private String action = "bills", token;
+    private String error = "Login ou mot de passe incorrect.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +43,18 @@ public class LoginActivity extends AppCompatActivity {
                     connectionRest.setToken(token);
                     connectionRest.execute("POST");
                     String token = (String) connectionRest.get();
-                    if(token.charAt(0)=='(') {
-                        Log.v("LoginActivity", token);
-                    }else {
+                    if(token.charAt(0) !='{') {
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         intent.putExtra("token", token);
                         startActivity(intent);
+                    } else {
+                        Log.v("LoginActivity", token);
+                        throw new InterruptedException();
                     }
 
                 } catch (JSONException | InterruptedException | ExecutionException e) {
+                    Toast toast = new Toast();
+                    toast.showToast(getApplicationContext(), error, LENGTH_LONG);
                     e.printStackTrace();
                 }
             }
